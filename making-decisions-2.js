30ed269d734844a327bbb832ed4a4df928b31569
Alec Wixom <atwixom@gmail.com>
2023-04-13 12:27:40 -0600

    update-new

--- making-decisions-2.js
+++ making-decisions-2.js
@@ -1,9 +1,22 @@
 
 let backpack = [];
+let belt = [];
+let equippedItems = [];
 
 backpack.push("sword", "shield", "food");
 
-console.log(backpack);
+let firstItem = backpack.shift();
 
-backpack.shift();
+belt.push(firstItem);
 
+let furCoat = "Fur Coat";
+
+backpack.unshift(furCoat);
+
+equippedItems.push(...backpack.splice(0, 1));
+
+backpack.push("flint", "blanket", "knife", "toothbrush");
+
+let backpackCount = backpack.length
+
+console.log(backpack,backpackCount, belt, equippedItems);
